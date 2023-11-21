8d964db4ebc230dc5910689d274e1774b3c891e4
Jose Manuel Martinez <josemarher027@gmail.com>
2023-11-21 10:08:32 +0100

    Añadida validacion del formulario contact

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
@@ -26,24 +27,40 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home');
 
 // Ruta para la pagina de contact
-Route::get('/contact', function () {
-    return Response::view('contact');
-});
+Route::get('/contacts/create', [ContactController::class, 'create'])
+    ->name('contacts.create');
+
+Route::post('/contacts', [ContactController::class, 'store'])
+    ->name('contacts.store');
+
+    
+
+
+
+
+
+
+
 
 // Devolver un json
-Route::post('/contact', function (Request $request) {
-    return Response::json(["message" => "hola"])->setStatusCode(400);
-});
+//Route::post('/contact', function (Request $request) {
+//    return Response::json(["message" => "hola"])->setStatusCode(400);
+//});
 
 // Query params
 /* Route::post('/contact', function (Request $request) {
     dd($request->query('test'));
 }); */
 
-// Rutas para el CSRF
+/* // Rutas para el CSRF
 Route::get('/change-password', function () {
     return Response::view('change-password');
 });
+
 Route::post('/change-password', function () {
-    return 'Password changed';
-});
+    if (Auth()->check()) {
+        return response("Password Changed");
+    } else {
+        return response("Not Authenticated", 401);
+    }
+}); */
